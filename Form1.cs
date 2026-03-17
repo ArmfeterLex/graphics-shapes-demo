0a175257dddfd597814b118f688fc637e58f7659
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-17 20:58:25 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wf351
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen pn = new Pen(Color.Red, 4);
+            Brush br = new SolidBrush(Color.Green);
+            g.Clear(SystemColors.Control);
+            switch (listBox1.SelectedIndex)
+            {
+                case 0: g.DrawLine(pn, 150, 40, 350, 180); break;
+                case 1: g.DrawRectangle(pn, 150, 30, 250, 150); break;
+                case 2: g.FillRectangle(br, 150, 30, 250, 150); break;
+                case 3: g.DrawEllipse(pn, 150, 30, 250, 150); break;
+                case 4: g.FillEllipse(br, 150, 30, 250, 150); break;
+                case 5: g.DrawPie(pn, 150, 30, 200, 200, 180, 225); break;
+                case 6: g.FillPie(br, 150, 30, 150, 150, 0, 45); break;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("wf351")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("wf351")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4dca9b48-bd38-4ddd-8dee-e7325710f38e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
